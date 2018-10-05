b4a7bd0df9b45ad86677549a661821bb306e9b5d
Martine Seignon <martine.seignon@jax.org>
2018-10-05 12:37:35 -0400

    tsne 2D plot (p1) layout section into a function

---
+++ modules/backend/tsnePlot2D.R
@@ -0,0 +1,22 @@
+## 2D tsne plot 
+tsne_2D_plot <- function(input, output, session){
+ ggplot(subsetData,
+  aes_string(x = input$dimension_x1, y = input$dimension_y1)) +
+ geom_point() +
+ geom_point(shape = 1,
+  size = 4,
+  color = "black") +
+ theme_bw() +
+ theme(axis.text.x = element_text(
+   angle = 90,
+   size = 12, 
+   vjust = 0.5),
+   axis.text.y = element_text(size = 12),
+   strip.text.x = element_text(size = 16),
+   strip.text.y = element_text(size = 14),
+   axis.title.x = element_text(face = "bold", size = 16),
+   axis.title.y = element_text(face = "bold", size = 16),
+   legend.position = "none") +
+ ggtitle(input$clusters1)
+}
+ 
