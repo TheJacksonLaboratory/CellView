ea3b9add1c76cb705062002317bfc219b5e6b850
Martine Seignon <martine.seignon@jax.org>
2018-10-09 10:54:28 -0400

    Rewrite heatmap server module function

--- modules/backend/heatmapPlot.R
+++ modules/backend/heatmapPlot.R
@@ -2,22 +2,26 @@
 ### CO-EXPRESSION HEATMAP ALL CLUSTERS ---- heatmap (h) chunk
 ### CO EXPRESSION TAB SELECTED HEATMAP ---- heatmap (h) chunk
 ## Heatmap chuncks--------------------
-heat_map <- function(input, output, session, cluster_cols=FALSE,annotation_col=NA){
-  pheatmap(
-  as.matrix(expression),
-  cluster_rows = TRUE,
-  cluster_cols = cluster_cols,
-  scale = 'row',
-  fontsize_row = 10,
-  labels_col = colnames(expression),
-  labels_row = dataTables$featuredata[rownames(expression), 'Associated.Gene.Name'],
-  show_rownames = TRUE,
-  annotation_col = annotation_col,
-  show_colnames = FALSE,
-  annotation_legend = TRUE,
-  breaks = seq(-6, 6, by = .12),
-  colorRampPalette(rev(brewer.pal(
-    n = 6, name = "RdBu")))(100)
+heat_map <- function(input, output, session,
+		     data = expression,
+		     cluster_cols = FALSE,
+		     annotation_col = NA) {
+  h <- pheatmap(
+    as.matrix(expression),
+    cluster_rows = TRUE,
+    cluster_cols = cluster_cols,
+    scale = 'row',
+    fontsize_row = 10,
+    labels_col = colnames(expression),
+    labels_row = dataTables$featuredata[rownames(expression), 'Associated.Gene.Name'],
+    show_rownames = TRUE,
+    annotation_col = annotation_col,
+    show_colnames = FALSE,
+    annotation_legend = TRUE,
+    breaks = seq(-6, 6, by = .12),
+    colorRampPalette(rev(brewer.pal(
+      n = 6, name = "RdBu")))(100)
   )
+  h
 }
 
