43c40ced3f7ba96baa786c6768e7d33988694187
Martine Seignon <martine.seignon@jax.org>
2018-10-09 13:37:01 -0400

    Changed 'tsne_2D_plot' to 'tsne_2dPplot' added 'data' argument to tsne_2d_plot function adjusted code chunk layouts tidyverse style

--- modules/backend/tsnePlot2D.R
+++ modules/backend/tsnePlot2D.R
@@ -8,41 +8,38 @@
 ##########################################################################
 
 ## 2D tsne plot ------------------------
-tsne_2D_plot <- function(input, output, session){
-  ggplot(subsetData,
-    aes_string(x = input$dimension_x1, y = input$dimension_y1)) +
-  geom_point() +
-  geom_point(shape = 1,
-    size = 4,
-    color = "black") +
-  theme_bw() +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    size = 12, 
-    vjust = 0.5),
-    axis.text.y = element_text(size = 12),
-    strip.text.x = element_text(size = 16),
-    strip.text.y = element_text(size = 14),
-    axis.title.x = element_text(face = "bold", size = 16),
-    axis.title.y = element_text(face = "bold", size = 16),
-    legend.position = "none") +
-  ggtitle(input$clusters1)
+tsne_2d_plot <- function(input, output, session, data){
+  p1 <-  
+    ggplot(data,aes_string(x = input$dimension_x1,
+                           y = input$dimension_y1)) +
+    geom_point() +
+    geom_point(shape = 1,
+               size = 4,
+               color = "black") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90,
+                                     size = 12, 
+                                     vjust = 0.5),
+          axis.text.y = element_text(size = 12),
+          strip.text.x = element_text(size = 16),
+          strip.text.y = element_text(size = 14),
+          axis.title.x = element_text(face = "bold", size = 16),
+          axis.title.y = element_text(face = "bold", size = 16),
+          legend.position = "none") + 
+    ggtitle(input$clusters1)
+  p1
 }
 
 ## plot theme -----------------------
-plot_theme <- function(input, output, session(){
+plot_theme <- function(input, output, session){
   theme_bw() +
-  theme(
-    axis.text.x = element_text(
-      angle = 90,
-      size = 12,
-      vjust = 0.5
-      ),
-    axis.text.y = element_text(size = 12),
-    strip.text.x = element_text(size = 16),
-    strip.text.y = element_text(size = 14),
-    axis.title.x = element_text(face = "bold", size = 16),
-    axis.title.y = element_text(face = "bold", size = 16),
-    legend.position = "none"
-    )
+  theme(axis.text.x = element_text(angle = 90,
+                                   size = 12,
+                                   vjust = 0.5),
+        axis.text.y = element_text(size = 12),
+        strip.text.x = element_text(size = 16),
+        strip.text.y = element_text(size = 14),
+        axis.title.x = element_text(face = "bold", size = 16),
+        axis.title.y = element_text(face = "bold", size = 16),
+        legend.position = "none")
 } 
