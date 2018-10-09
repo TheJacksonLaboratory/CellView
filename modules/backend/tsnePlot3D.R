23a1410f5eac2aac6fa0dcf4ea7bc8f4456a0b11
Martine Seignon <martine.seignon@jax.org>
2018-10-09 14:27:45 -0400

    Changed tsne_3D_plot to tsne_3d_plot Added data, title, color, size argument to function Added layout(p) to function

--- modules/backend/tsnePlot3D.R
+++ modules/backend/tsnePlot3D.R
@@ -1,21 +1,20 @@
 ## 3D tsne plots layout-----------------------------------
-tsne_3D_layout <- function(input, output, session){
-plot_ly(
-  tsne.data,
-  x = ~ V1,
-  x = ~ V2,
-  z = ~ V3,
-  type = "scatter3d",
-  color =  ~ dbCluster,
-  hoverinfo = "text",
-  text = paste('Cluster:', tsne.data$dbCluster),
-  mode = 'markers',
-  marker = 
-   list(
-    line = list(width = 0),
-    size = rep(10, nrow(tsne.data)),
-    sizeref = 3
-   )
- )
+tsne_3d_layout <- function(input, output, session, data, title, color, size){
+  p <-   
+    plot_ly(data,
+            x = ~ V1,
+            x = ~ V2,
+            z = ~ V3,
+            type = "scatter3d",
+            color = color,
+            hoverinfo = "text",
+            text = paste('Cluster:', data$dbCluster),
+            mode = 'markers',
+            marker = list(
+                          line = list(width = 0),
+                          size = size,
+                          sizeref = 3)
+  )
+  layout(p, title)
 }  
 
